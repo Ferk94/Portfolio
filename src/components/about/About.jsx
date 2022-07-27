3ffc3d6b4148e5bf2fbcc11c52e0820ecb9a2515
Fernando <Fernando.Kaganovicz@gmail.com>
2022-07-27 16:44:46 -0300

    last changes 2.3

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ function About() {
         <h3>Desarrollador FullStack</h3>
         <div className='about-text'>
           Me dedico al desarrollo web hace 2 años, con buenos conocimientos tanto de backend como de frontend. Aceleré mi proceso de aprendizaje en la academia de <a href='https://www.soyhenry.com/' target='_blank'>Soy Henry</a>.
-          Soy una persona didáctica, comunicativa y responsable que busca alcanzar sus metas ofreciendo su compromiso, lealtad y conociminetotos.
+          Soy una persona didáctica, comunicativa y responsable que busca alcanzar sus metas ofreciendo su compromiso, lealtad y conocimientos.
           Busco mi primer empleo en el mundo IT, si bien tengo trabajos realizados Full-Stack de manera Free Lance que a continuación podrán observar. 
         </div>
       </div>
